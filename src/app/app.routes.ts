096e3a762e3b7bf1feb4d925d36687507029cdd5
Ghislaine Aybram <ghislaine.aybram@gmail.com>
2025-07-16 18:28:39 +0200

    font and color added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { ContactComponent } from './features/contact/contact.component';
 import { PrivacyComponent } from './features/privacy/privacy.component';
 import { AboutComponent } from './features/homepage/about/about.component';
 import { HomepageComponent } from './features/homepage/homepage/homepage.component';
+import { AdminComponent } from './features/admin/admin.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent},
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'legal', component: LegalComponent},
     { path: 'privacy', component: PrivacyComponent},
     { path: 'contact', component: ContactComponent},
+    { path: 'admin', component: AdminComponent},
 ];
--- src/app/features/homepage/pillars/pillars.component.ts
+++ src/app/features/homepage/pillars/pillars.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Pillar } from '../../../models/pillar';
 
 @Component({
   selector: 'app-pillars',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pillars.component.html',
   styleUrl: './pillars.component.scss'
 })
 export class PillarsComponent {
+  pillars: Pillar[] = [];
 
+  ngOnInit(): void {
+    this.pillars = [
+      {
+        name: 'Compléter ses revenus',
+        description: 'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.'
+      },
+      {
+        name: 'Protéger ses proches',
+        description: 'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.'
+      },
+      {
+        name: 'Préparer sa retraite',
+        description: 'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.'
+      },
+      {
+        name: 'Optimiser sa fiscalité',
+        description: 'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.'
+      },
+    ]
+  }
 }
